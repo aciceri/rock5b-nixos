f535f96921b6d6452ecac4ac4d2a66fc125e76a9
Andrea Ciceri <andrea.ciceri@autistici.org>
2022-12-09 15:00:29 +0100

    [CI] Fix?

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
       evalConfig = import "${nixpkgs}/nixos/lib/eval-config.nix";
       buildConfig = hostSystem: config:
         superLib.evalConfig {
-          system = "aarch64-linux";
+          system = hostSystem;
           modules = [
             config
             ./modules/kernel/cross.nix
