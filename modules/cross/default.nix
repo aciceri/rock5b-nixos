7da40bebe3a687eabaabb43ec5ae6cf06f97f3e8
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-03-11 14:05:48 +0100

    Fixed cross compilation

--- modules/cross/default.nix
+++ modules/cross/default.nix
@@ -5,29 +5,17 @@
 }: let
   inherit (config.nixpkgs) localSystem;
   selectedPlatform = lib.systems.elaborate "aarch64-linux";
-  isCross = localSystem != selectedPlatform.system; # FIXME
-in
-  lib.mkMerge [
-    # {
-    #   nixpkgs.overlays = [
-    #     (final: super: {
-    #       # Workaround for modules expected by NixOS not being built more often than not.
-    #       # TODO Do we still need this?
-    #       makeModulesClosure = x: super.makeModulesClosure (x // {allowMissing = true;});
-    #     })
-    #   ];
-    # }
-    # (lib.mkIf isCross {
-    #   # Some filesystems (e.g. zfs) have some trouble with cross (or with BSP kernels?) here.
-    #   boot.supportedFilesystems = lib.mkForce ["vfat"];
+  isCross = localSystem != selectedPlatform.system;
+in (lib.mkIf isCross {
+      # Some filesystems (e.g. zfs) have some trouble with cross (or with BSP kernels?) here.
+      boot.supportedFilesystems = lib.mkForce ["vfat"];
 
-    #   nixpkgs.crossSystem = builtins.trace ''
-    #     Building with a crossSystem?: ${selectedPlatform.system} != ${localSystem.system} → ${
-    #       if isCross
-    #       then "we are"
-    #       else "we're not"
-    #     }.
-    #            crossSystem: config: ${selectedPlatform.config}''
-    #   selectedPlatform;
-    # })
-  ]
+      nixpkgs.crossSystem = builtins.trace ''
+        Building with a crossSystem?: ${selectedPlatform.system} != ${localSystem.system} → ${
+          if isCross
+          then "we are"
+          else "we're not"
+        }.
+               crossSystem: config: ${selectedPlatform.config}''
+      selectedPlatform;
+    })
