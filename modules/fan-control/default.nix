36fbe94fca2d6690fa1502f6a51fc39e57ac89ed
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-03-11 11:44:10 +0100

    Default config for `rock5b-fan-control`

--- modules/fan-control/default.nix
+++ modules/fan-control/default.nix
@@ -4,18 +4,74 @@
   pkgs,
   ...
 }: {
-  systemd.services.fan-control = {
-    description = "Fan control for Radxa Rock5B";
-    after = ["networking.target"];
-    startLimitBurst = 0;
-    startLimitIntervalSec = 60;
-    serviceConfig = {
-      Type = "forking";
-      PIDFile = "/run/fan-control.pid";
-      ExecStart = "${pkgs.fan-control}/bin/fan-control -d -p /run/fan-control.pid";
-      Restart = "always";
-      RestartSec = "2";
-      TimeoutStopSec = "15";
+  options.rock5b-fan-control = {
+    enable = lib.mkEnableOption {
+      default = true;
     };
+    settings = lib.mkOption {
+      description = "Settings for Radxa Rock 5B";
+      type = lib.types.attrs;
+      default = {
+        pwmchip= -1;
+        gpio= 0;
+        pwm-period= 10000;
+        temp-map = [
+          {
+            temp= 40;
+            duty= 0;
+            duration= 20;
+          }
+          {
+            temp= 44;
+            duty= 55;
+            duration= 25;
+          }
+          {
+            temp= 49;
+            duty= 60;
+            duration= 35;
+          }
+          {
+            temp= 54;
+            duty= 70;
+            duration= 45;
+          }
+          {
+            temp= 59;
+            duty= 80;
+            duration= 60;
+          }
+          {
+            temp= 64;
+            duty= 90;
+            duration= 120;
+          }
+          {
+            temp= 67;
+            duty= 100;
+            duration= 180;
+          }
+        ];
+      };
+    };
+  };
+
+  config = lib.mkIf config.rock5b-fan-control.enable {
+    systemd.services.fan-control = {
+      description = "Fan control for Radxa Rock5B";
+      after = ["networking.target"];
+      startLimitBurst = 0;
+      startLimitIntervalSec = 60;
+      serviceConfig = {
+        Type = "forking";
+        PIDFile = "/run/fan-control.pid";
+        ExecStart = "${pkgs.fan-control}/bin/fan-control -d -p /run/fan-control.pid";
+        Restart = "always";
+        RestartSec = "2";
+        TimeoutStopSec = "15";
+      };
+    };
+
+    environment.etc."fan-control.json".text = builtins.toJSON config.rock5b-fan-control.settings;
   };
 }
--- modules/kernel/default.nix
+++ modules/kernel/default.nix
@@ -11,6 +11,7 @@
     "earlyprintk"
   ];
   boot.initrd.availableKernelModules = lib.mkForce [ "usbhid" "md_mod" "raid0" "raid1" "raid10" "raid456" "ext2" "ext4" "sd_mod" "sr_mod" "mmc_block" "uhci_hcd" "ehci_hcd" "ehci_pci" "ohci_hcd" "ohci_pci" "xhci_hcd" "xhci_pci" "usbhid" "hid_generic" "hid_lenovo" "hid_apple" "hid_roccat" "hid_logitech_hidpp" "hid_logitech_dj" "hid_microsoft" "hid_cherry" ];
+  # boot.blacklistedKernelModules = [ "sata_nv" ];
   nixpkgs.overlays = [
     (self: _: {
       linuxPackages_rock5 = self.linuxPackagesFor self.linux_rock5b;
