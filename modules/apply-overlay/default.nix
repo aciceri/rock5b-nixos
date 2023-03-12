8cc911e4b29a4e6fb79861306cfc36eff29dee15
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-03-12 01:40:01 +0100

    Formatting

--- modules/apply-overlay/default.nix
+++ modules/apply-overlay/default.nix
@@ -10,12 +10,13 @@
   dynamicOverlay =
     if isCross
     then
-      (self: super: with (rock5bFlake.packages.${localSystem.system}); {
-        inherit linux-rock5b;
-        fan-control = fan-control-rock5b;
-        kodi-rock5b = kodi;
-        mesa = panfork;
-      })
+      (self: super:
+        with (rock5bFlake.packages.${localSystem.system}); {
+          inherit linux-rock5b;
+          fan-control = fan-control-rock5b;
+          kodi-rock5b = kodi;
+          mesa = panfork;
+        })
     else rock5bFlake.overlays.default;
 in {
   nixpkgs.overlays = [
