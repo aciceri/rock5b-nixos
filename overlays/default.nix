e5573706183ec72dca5f7b1f6f925869e9509b40
Pengcheng Xu <i@jsteward.moe>
2024-01-19 18:28:18 +0100

    Use input nixpkgs to generate overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,8 +2,8 @@
   imports = [
     inputs.flake-parts.flakeModules.easyOverlay
   ];
-  perSystem = {config, ...}: {
-    overlayAttrs = {
+  perSystem = {config, final, ...}: {
+    overlayAttrs = builtins.mapAttrs (name: value: final.callPackage value {}) {
       mesa = config.packages.mesa-panfork;
       kodi-rock5b = config.packages.kodi;
       fan-control-rock5b = config.packages.fan-control;
